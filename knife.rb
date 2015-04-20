713e1997d6078da16ad92af0de8cb489fd38b2f9
Mat Brennan <loadx.mat@gmail.com>
2015-04-21 09:32:47 +1000

    Initial commit - kitchenci and knife config

---
+++ knife.rb
@@ -0,0 +1,6 @@
+current_dir = File.dirname(__FILE__)
+
+cookbook_path File.join(current_dir, 'cookbooks')
+cookbook_copyright "Mat Brennan"
+cookbook_license "mit"
+cookbook_email "loadx.mat@gmail.com"
